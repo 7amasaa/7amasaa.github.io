04cdc40b80d2baaf54de433aa3f6dfb3d5a9ccfc
7amasaa <37583722+7amasaa@users.noreply.github.com>
2018-09-08 21:02:57 +0200

    load

--- task/4/js/java.js
+++ task/4/js/java.js
@@ -1,29 +1,17 @@
-function equation() {
-	"use strict";
-var a = document.getElementById('firstNumber').value;
-var b = document.getElementById('secondNumber').value;
-var c = document.getElementById('thirdNumber').value;
-var d = (a * b) / c;
-var answer = Math.sqrt(d);
-	
-	
-	
-	document.getElementById('answer').value = answer;
-	
-	
-}
+   
 
 $(function () {
 	"use strict";
     $(".load").slice(0, 2).show();
     $("#loadMore").on('click', function (e) {
         e.preventDefault();
-        $(".load:hidden").slice(0, 2).slideDown();
+        $(".load:hidden").slice(0, 2).fadeIn(500);
         if ($(".load:hidden").length === 0) {
-            $("#loadMore").fadeOut('slow');
+            $("#loadMore").fadeOut(1000);
         }
         $('html,body').animate({
             scrollTop: $(this).offset().top
         }, 1500);
     });
 });
+
